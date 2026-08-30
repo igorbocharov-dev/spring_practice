5a60e02f5c47308caf767b98905409c8c5f808a1
Igor <igor.bocharov.77@gmail.com>
2026-08-31 00:26:20 +0300

    clear imports

--- src/main/java/com/practice/spring/controller/note/NoteController.java
+++ src/main/java/com/practice/spring/controller/note/NoteController.java
@@ -12,8 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
